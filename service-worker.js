2c7146d4aaf4e22217b08961cd3534c5bdccda50
IsaDias2000 <isaias.dias.baranhuk@gmail.com>
2025-04-09 16:27:33 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-
 const CACHE_NAME = "gestao-financeira-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./script.js",
-  "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
