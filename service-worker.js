cb199f090cd983db1cce05c66eb0df5eb3ac78ea
IsaDias2000 <isaias.dias.baranhuk@gmail.com>
2025-04-09 17:17:06 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,17 @@ const FILES_TO_CACHE = [
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
