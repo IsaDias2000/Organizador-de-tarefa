147989606694be9f91b572847c96f7cf8d7b4107
IsaDias2000 <isaias.dias.baranhuk@gmail.com>
2025-04-09 12:09:12 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = "gestao-financeira-v1";
+const CACHE_NAME = "organizador-tarefas-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
+  "./script.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
