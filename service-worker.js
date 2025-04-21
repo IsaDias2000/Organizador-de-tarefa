3d4937b0625a3eccfda6504d89b346653783768f
IsaDias2000 <isaias.dias.baranhuk@gmail.com>
2025-04-21 20:16:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'financas-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', (e) => {
+  console.log('Service Worker installed');
+  e.waitUntil(
+    caches.open('finance-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        'https://cdn.jsdelivr.net/npm/chart.js'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
